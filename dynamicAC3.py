2fd9472ec19fa1f1c59def18f260efcb6522155a
Nikita Yurkov <yurko006@umn.edu>
2015-11-09 18:42:54 -0600

    Working AC3 implementation

--- dynamicAC3.py
+++ dynamicAC3.py
@@ -1,22 +1,8 @@
 import functools
 from functools import reduce
 from testRead import *
+from enum import Enum
 
-# GROUP
-# Trey Leuenberger
-# Nikita Yurkov
-# Colin Heim
-# Dillon Littlefield
-
-# This is demonstrating a "class" implementation of AC3. You can accomplish the same with lists. For the project, you can choose either.
-
-# The primary problem set-up consists of "variables" and "constraints":
-#   "variables" are a dictionary of constraint variables (of type ConstraintVar), example variables['A1']
-#   "constraints" are a set of binary constraints (of type BinaryConstraint)
-
-# First, Node Consistency is achieved by passing each UnaryConstraint of each variable to nodeConsistent().
-# Arc Consistency is achieved by passing "constraints" to Revise().
-# AC3 is not fully implemented, Revise() needs to be repeatedly called until all domains are reduced to a single value
 
 class ConstraintVar:
 	# instantiation example: ConstraintVar( [1,2,3],'A1' )
@@ -73,37 +59,6 @@ def allDiffTC( constraints, v ):
                 if ( i != j != k ) :
                     constraints.append(TernaryConstraint( v[i],v[j], v[k], fn ))
 
-# def setUpKenKen( variables, constraints ):
-#     # This setup is applicable to KenKen and Sudoku. For this example, it is a 3x3 board with each domain initialized to {1,2,3}
-#     # The VarNames list can then be used as an index or key into the dictionary, ex. variables['A1'] will return the ConstraintVar object
-#
-#     # Note that I could accomplish the same by hard coding the variables, for example ...
-#     # A1 = ConstraintVar( [1,2,3],'A1' )
-#     # A2 = ConstraintVar( [1,2,3],'A2' ) ...
-#     # constraints.append( BinaryConstraint( A1, A2, lambda x,y: x != y ) )
-#     # constraints.append( BinaryConstraint( A2, A1, lambda x,y: x != y ) ) ...
-#     #   but you can see how tedious this would be.
-#
-#     rows = ['A','B','C', 'D']
-#     cols = ['1','2','3', '4']
-#     varNames = [ x+y for x in rows for y in cols ]
-#     for var in varNames:
-#         variables[var] = ConstraintVar( [1,2,3,4],var )
-#
-#     # establish the allDiff constraint for each column and each row
-#     # for AC3, all constraints would be added to the queue
-#
-#     # for example, for rows A,B,C, generate constraints A1!=A2!=A3, B1!=B2...
-#     for r in rows:
-#         aRow = []
-#         for k in variables.keys():
-#             if ( str(k).startswith(r) ):
-# 		#accumulate all ConstraintVars contained in row 'r'
-#                 aRow.append( variables[k] )
-# 	#add the allDiff constraints among those row elements
-#       #  for i in aRow:
-#
-#         allDiff( constraints, aRow )
 
 def setUpKenKen( variables, constraints, size ):
     # This setup is applicable to KenKen and Sudoku. For this example, it is a 3x3 board with each domain initialized to {1,2,3}
@@ -153,10 +108,6 @@ def setUpKenKen( variables, constraints, size ):
                 aCol.append( variables[k] )
         allDiff( constraints, aCol )
 
-
-
-
-
     # for example, for cols 1,2,3 (with keys A1,B1,C1 ...) generate A1!=B1!=C1, A2!=B2 ...
     for c in cols:
         aCol = []
@@ -168,13 +119,10 @@ def setUpKenKen( variables, constraints, size ):
                 aCol.append( variables[k] )
         allDiff( constraints, aCol )
 
-#--------------------------------------------------------------------------------------------
-#########################            COMPLETE REVISE               ##########################
 
 def Revise( bc, variables ):
 	# The Revise() function from AC-3, which removes elements from var1 domain, if not arc consistent
 	# A single BinaryConstraint instance is passed in to this function.
-	# MISSSING the part about returning sat to determine if constraints need to be added to the queue
 
     # copy domains for use with iteration (they might change inside the for loops)
     dom1 = list(bc.var1.domain)
@@ -239,14 +187,50 @@ def printDomains( vars, size):
         if ( 0 == count % size ):
             print(' ')
 
+def isSolved(vars):
+    for i in vars:
+        if(len(vars[i].domain) > 1):
+            return 0
+        elif(len(vars[i].domain) == 0):
+            return 2
+    return 1
+
+def makeGuess(variables, backTrackStack):
+
+
+def backTrackingSearch(variables, constraints, constraintsTC, size):
+    backTrackStack = []
+    while isSolved(variables) != 1:
+        if isSolved(variables) == 2:
+            #if there are no solutions
+            print("MADE A BAD GUESS ABORTING\n")
+            printDomains(variables, size)
+            return
+        else:
+            #if there is still more to be found
+            makeGuess(variables, backTrackStack)
+            AC3Revise(variables, constraints, constraintsTC, size)
+    print("problem solved\n")
+    printDomains(variables, size)
+
+
+
+
+def AC3Revise(variables, constraints, constraintsTC, size):
+    print("Initial domains\n")
+    printDomains(variables, size)
+    for c in constraints:
+        Revise( c , variables)
+    for tc in constraintsTC:
+        ReviseTC( tc, variables )
+    print("Final domains\n")
+    printDomains(variables, size)
 
 
 
 def AC3(listNumber):
 
     data = readKenKen(listNumber)
-    print(data)
-
 
     # create a dictionary of ConstraintVars keyed by names in VarNames.
     variables = dict()
@@ -254,9 +238,6 @@ def AC3(listNumber):
     constraintsTC = []
     size = int(data[0][0])
     setUpKenKen( variables, constraints, size)
-
-    print("initial domains \n")
-    printDomains( variables,size )
 
     for i in data[1:]:
         if(len(i) == 4):
@@ -281,47 +262,35 @@ def AC3(listNumber):
                 constraints.append(BinaryConstraint(variables[v1], variables[v2], lambda x,y: x*y == tempPr2 and x != y))
                 constraints.append(BinaryConstraint(variables[v2], variables[v1], lambda x,y: x*y == tempPr2 and x != y))
         elif(len(i) == 5):
-            pr = i[0]
             op = i[1]
             v1 = i[2]
             v2 = i[3]
             v3 = i[4]
+            temp = int(i[0])
             if op == '+':
-                tempPr = int(pr)
-                constraintsTC.append( TernaryConstraint(variables [v1], variables[v3], variables[v2], lambda x,y,z: x+y+z == tempPr and x != y != z))
-                constraintsTC.append( TernaryConstraint(variables [v1], variables[v2], variables[v3], lambda x,y,z: x+y+z == tempPr and x != y != z))
-                constraintsTC.append( TernaryConstraint(variables [v3], variables[v1], variables[v2], lambda x,y,z: x+y+z == tempPr and x != y != z))
-                constraintsTC.append( TernaryConstraint(variables [v3], variables[v2], variables[v1], lambda x,y,z: x+y+z == tempPr and x != y != z))
-                constraintsTC.append( TernaryConstraint(variables [v2], variables[v1], variables[v3], lambda x,y,z: x+y+z == tempPr and x != y != z))
-                constraintsTC.append( TernaryConstraint(variables [v2], variables[v3], variables[v1], lambda x,y,z: x+y+z == tempPr and x != y != z))
+                constraintsTC.append( TernaryConstraint(variables [v1], variables[v2], variables[v3], lambda x,y,z: x+y+z == temp ))
+                constraintsTC.append( TernaryConstraint(variables [v1], variables[v3], variables[v2], lambda x,y,z: x+y+z == temp ))
+                constraintsTC.append( TernaryConstraint(variables [v2], variables[v1], variables[v3], lambda x,y,z: x+y+z == temp ))
+                constraintsTC.append( TernaryConstraint(variables [v2], variables[v3], variables[v1], lambda x,y,z: x+y+z == temp ))
+                constraintsTC.append( TernaryConstraint(variables [v3], variables[v1], variables[v2], lambda x,y,z: x+y+z == temp ))
+                constraintsTC.append( TernaryConstraint(variables [v3], variables[v2], variables[v1], lambda x,y,z: x+y+z == temp ))
             elif op == '*':
-                tempPr = int(pr)
-                constraintsTC.append( TernaryConstraint(variables [v1], variables[v3], variables[v2], lambda x,y,z: x*y*z == tempPr and x != y != z))
-                constraintsTC.append( TernaryConstraint(variables [v1], variables[v2], variables[v3], lambda x,y,z: x*y*z == tempPr and x != y != z))
-                constraintsTC.append( TernaryConstraint(variables [v3], variables[v1], variables[v2], lambda x,y,z: x*y*z == tempPr and x != y != z))
-                constraintsTC.append( TernaryConstraint(variables [v3], variables[v2], variables[v1], lambda x,y,z: x*y*z == tempPr and x != y != z))
-                constraintsTC.append( TernaryConstraint(variables [v2], variables[v1], variables[v3], lambda x,y,z: x*y*z == tempPr and x != y != z))
-                constraintsTC.append( TernaryConstraint(variables [v2], variables[v3], variables[v1], lambda x,y,z: x*y*z == tempPr and x != y != z))
+                constraintsTC.append( TernaryConstraint(variables [v1], variables[v2], variables[v3], lambda x,y,z: x*y*z == temp ))
+                constraintsTC.append( TernaryConstraint(variables [v1], variables[v3], variables[v2], lambda x,y,z: x*y*z == temp ))
+                constraintsTC.append( TernaryConstraint(variables [v2], variables[v1], variables[v3], lambda x,y,z: x*y*z == temp ))
+                constraintsTC.append( TernaryConstraint(variables [v2], variables[v3], variables[v1], lambda x,y,z: x*y*z == temp ))
+                constraintsTC.append( TernaryConstraint(variables [v3], variables[v1], variables[v2], lambda x,y,z: x*y*z == temp ))
+                constraintsTC.append( TernaryConstraint(variables [v3], variables[v2], variables[v1], lambda x,y,z: x*y*z == temp ))
 
+    AC3Revise(variables, constraints, constraintsTC, size)
 
-
-
-
-    '''
-    constraintsTC.append( TernaryConstraint(variables ['A1'], variables['A2'], variables['B1'], lambda x,y,z: x*y*z == 8 and x != y != z ))
-    constraintsTC.append( TernaryConstraint(variables ['A1'], variables['B1'], variables['A2'], lambda x,y,z: x*y*z == 8 and x != y != z))
-    constraintsTC.append( TernaryConstraint(variables ['A2'], variables['A1'], variables['B1'], lambda x,y,z: x*y*z == 8 and x != y != z))
-    constraintsTC.append( TernaryConstraint(variables ['A2'], variables['B1'], variables['A1'], lambda x,y,z: x*y*z == 8 and x != y != z))
-    constraintsTC.append( TernaryConstraint(variables ['B1'], variables['A1'], variables['A2'], lambda x,y,z: x*y*z == 8 and x != y != z))
-    constraintsTC.append( TernaryConstraint(variables ['B1'], variables['A2'], variables['A1'], lambda x,y,z: x*y*z == 8 and x != y != z))
-    '''
-    for c in constraints:
-        Revise( c , variables)
-    for tc in constraintsTC:
-        ReviseTC( tc, variables )
-    print("Final variables\n")
-    printDomains( variables,size )
-
+    if(isSolved(variables) == 1):
+        print("Problem done\n")
+    else:
+        print("Problem not done, starting backtracking")
+        '''
+        backTrackingSearch(variables, constraints, constraintsTC, size)
+        '''
 
 
 
